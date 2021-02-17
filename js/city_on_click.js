1fb9bc4574b002b79bc826299a7cefa4e547ef00
Mikhail-wd <Supalis913@yahoo.com>
2021-02-17 19:34:48 +0700

    Правки и настройка скриптов.
    Доделал верстку для десктопа и мобильного телефона. Добавли скрипты на удаление и добавление табличек.

--- js/city_on_click.js
+++ js/city_on_click.js
@@ -1,7 +1,17 @@
 function citySelect(){
 
 	var city = document.getElementById("search").value;
+	if(city==""){
+
+		document.getElementById("titleCity").innerHTML = "Так ты и не искал.";
+		document.getElementsByClassName("feed")[0].style.display="none";
+		document.getElementsByClassName("feed")[1].style.display="none";
+		document.getElementsByClassName("feed")[2].style.display="none";
+		document.getElementsByClassName("feed")[3].style.display="none";
+
+	} else {
 	newData(city);
+	};
 
 };
 
@@ -9,13 +19,31 @@ var counts=0;
 
 function favorite(){
 
+	if(indexOfCitys.includes(document.getElementById("cityID").innerHTML*1)) {
+
+		return(alert("Уже есть."));
+
+	} else {
+
 	var rightCol=document.getElementById("right-col");
 	var cityName=document.getElementById("titleCity").innerText;
 	var cityTemp=document.getElementById("temp").innerText;
+	var icon=document.getElementById("png").src;
+	var docID=document.getElementById("cityID").innerHTML;
 
-	rightCol.insertAdjacentHTML("beforeend",`<div class="city-like bordr"><h1>`+cityName+`</h1><img src="img/close-button.svg" class="cancel-x"><h1>`+cityTemp+`</h1><img src="img/cog-solid-black.svg" class="cog-right"></div>`);
-	return(counts++);
+	rightCol.insertAdjacentHTML("beforeend",`<div id="`+docID+`" class="city-like bordr"><h1>`+cityName+`</h1><img onclick="deleteTemp(`+(docID*1)+`)" src="img/close-button.svg" class="cancel-x"><h1>`+cityTemp+`</h1><img src=`+icon+` class="cog-right"></div>`);
+	return(indexOfCitys.push(docID*1));
+	}
 };
+
+function deleteTemp(valueOfID=0){
+
+	var cityID = document.getElementById(valueOfID);
+	cityID.parentNode.removeChild(cityID);
+	var indexToDelete=indexOfCitys.indexOf(valueOfID);
+
+	return(delete indexOfCitys[indexToDelete]);
+}
 
 var counts;
 function deleteFavorite(){
--- js/city_on_load.js
+++ js/city_on_load.js
@@ -1,3 +1,5 @@
+var indexOfCitys =[];
+
 function newData(city="лондон") {
 	var newCity = new XMLHttpRequest();
 	newCity.open ("GET", "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=45dfa3308548a42f21500c760482c10e", false);
@@ -18,6 +20,9 @@ function newData(city="лондон") {
 		document.getElementsByClassName("feed")[2].style.display="block";
 		document.getElementsByClassName("feed")[3].style.display="block";
 		document.getElementById("titleCity").innerHTML = city.charAt(0).toUpperCase()+city.slice(1);
+		//http://openweathermap.org/img/wn/10d@2x.png
+		document.getElementById("png").src = "http://openweathermap.org/img/wn/"+newCityJson.weather[0].icon+"@2x.png";
+		document.getElementById("cityID").innerHTML = newCityJson.id;
 		document.getElementById("temp").innerHTML = newCityJson.main.temp;
 		document.getElementById("feelsLike").innerHTML = newCityJson.main.feels_like;
 		document.getElementById("pres").innerHTML = newCityJson.main.pressure;
