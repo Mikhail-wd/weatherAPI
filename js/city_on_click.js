a13b1b05725a882f896f2b7a6a4b2bc8d9231963
Mikhail-wd <Supalis913@yahoo.com>
2021-02-19 15:12:58 +0700

    Добавление данных в localStorage
    Добавление скриптов.

--- js/city_on_click.js
+++ js/city_on_click.js
@@ -3,7 +3,7 @@ function citySelect(){
 	var city = document.getElementById("search").value;
 	if(city==""){
 
-		document.getElementById("titleCity").innerHTML = "Так ты и не искал.";
+		document.getElementById("titleCityLeft").innerHTML = "Так ты и не искал.";
 		document.getElementsByClassName("feed")[0].style.display="none";
 		document.getElementsByClassName("feed")[1].style.display="none";
 		document.getElementsByClassName("feed")[2].style.display="none";
@@ -15,37 +15,33 @@ function citySelect(){
 
 };
 
-var counts=0;
-
 function favorite(){
 
-	if(indexOfCitys.includes(document.getElementById("cityID").innerHTML*1)) {
+	if(indexOfCitys.includes(cityName=document.getElementById("search").value)) {
 
 		return(alert("Уже есть."));
 
 	} else {
-
-	var rightCol=document.getElementById("right-col");
-	var cityName=document.getElementById("titleCity").innerText;
-	var cityTemp=document.getElementById("temp").innerText;
-	var icon=document.getElementById("png").src;
-	var docID=document.getElementById("cityID").innerHTML;
-
-	rightCol.insertAdjacentHTML("beforeend",`<div id="`+docID+`" class="city-like bordr"><h1>`+cityName+`</h1><img onclick="deleteTemp(`+(docID*1)+`)" src="img/close-button.svg" class="cancel-x"><h1>`+cityTemp+`</h1><img src=`+icon+` class="cog-right"></div>`);
-	return(indexOfCitys.push(docID*1));
+		var rightCol=document.getElementById("right-col");
+		var cityName=document.getElementById("titleCityLeft").innerText;
+		var cityTemp=document.getElementById("tempLeft").innerText;
+		var icon=document.getElementById("png").src;
+		var docID=document.getElementById("cityID").innerHTML;
+		rightCol.insertAdjacentHTML("beforeend",`<div id="`+cityName+`" class="city-like `+cityName+` bordr"><h1>`+cityName+" "+`</h1><img onclick="deleteTemp('`+cityName+`')" src="img/close-button.svg" class="cancel-x"><h1>`+cityTemp+`</h1><img src=`+icon+` class="cog-right"></div>`);
+		
+		citysList=citysList+cityName+",";
+		localStorage.setItem("citys_on_exit",citysList);
 	}
 };
 
-function deleteTemp(valueOfID=0){
+function deleteTemp(valueOfID=""){
 
 	var cityID = document.getElementById(valueOfID);
 	cityID.parentNode.removeChild(cityID);
 	var indexToDelete=indexOfCitys.indexOf(valueOfID);
 
-	return(delete indexOfCitys[indexToDelete]);
+	citysList=citysList.replace(valueOfID+",", "")
+
+	localStorage.setItem("citys_on_exit",citysList);
+
 }
-
-var counts;
-function deleteFavorite(){
-
-};
