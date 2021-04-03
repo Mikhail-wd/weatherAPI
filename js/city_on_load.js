f33e279214a555b16a5ace61aedc78d6eac74877
Mikhail-wd <Supalis913@yahoo.com>
2021-04-03 14:03:30 +0700

    merge from dev
    перенос

--- js/city_on_load.js
+++ js/city_on_load.js
@@ -1,4 +1,33 @@
+<<<<<<< Updated upstream
 function newData(city="лондон") {
+=======
+if (localStorage.getItem("citys_on_exit")){
+
+	var citysList = localStorage.getItem("citys_on_exit");
+	var indexOfCitys = citysList.split(",");
+
+} else {
+
+	localStorage.setItem("citys_on_exit","");
+	var citysList = localStorage.getItem("citys_on_exit");
+	var indexOfCitys = citysList.split(",");
+
+	function firstLoad(){
+		newData();
+		plates();
+	};
+};
+
+var citysList = localStorage.getItem("citys_on_exit");
+var indexOfCitys = citysList.split(",");
+
+function firstLoad(){
+	newData();
+	plates();
+};
+
+function newData(city="") {
+>>>>>>> Stashed changes
 	var newCity = new XMLHttpRequest();
 	newCity.open ("GET", "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=45dfa3308548a42f21500c760482c10e", false);
 	newCity.send (null);
@@ -16,8 +45,14 @@ function newData(city="лондон") {
 		document.getElementsByClassName("feed")[1].style.display="block";
 		document.getElementsByClassName("feed")[2].style.display="block";
 		document.getElementsByClassName("feed")[3].style.display="block";
+<<<<<<< Updated upstream
 		document.getElementById("titleCity").innerHTML = city.charAt(0).toUpperCase()+city.slice(1);
 		document.getElementById("temp").innerHTML = newCityJson.main.temp;
+=======
+		document.getElementById("titleCityLeft").innerHTML = city.charAt(0).toUpperCase()+city.slice(1);
+		document.getElementById("png").src = "http://openweathermap.org/img/wn/"+newCityJson.weather[0].icon+"@2x.png";
+		document.getElementById("tempLeft").innerHTML = newCityJson.main.temp;
+>>>>>>> Stashed changes
 		document.getElementById("feelsLike").innerHTML = newCityJson.main.feels_like;
 		document.getElementById("pres").innerHTML = newCityJson.main.pressure;
 		document.getElementById("wind").innerHTML = windDerect(newCityJson.wind.deg);
